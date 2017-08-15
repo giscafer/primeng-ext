829ffa3a5cae4737cbef74849d8512bc6528012f
giscafer <giscafer@outlook.com>
2017-08-15 13:53:54 +0800

    update tabview

--- tabview/tabview.ts
+++ tabview/tabview.ts
@@ -2,10 +2,14 @@
  * ui-tabview组件，基于primeng p-tabview实现
  * Created by giscafer on 2017-07-19.
  */
-import {Component, OnInit, Input, Output, AfterViewInit, NgModule, EventEmitter, ViewChild} from "@angular/core";
+import {
+    Component, OnInit, Input, Output, AfterViewInit, NgModule, EventEmitter, ViewChild,
+    Renderer
+} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {UITabPanel} from "app/shared/ui/tabview/tab-panel";
 import {TabViewModule} from "primeng/components/tabview/tabview";
+import {OverlayPanelModule, MenuModule} from 'primeng/primeng';
 
 @Component({
     templateUrl: "./tabview.html",
@@ -26,9 +30,46 @@ export class UITabView implements OnInit,AfterViewInit {
     @Output()
     tabClose: EventEmitter<any> = new EventEmitter<any>();
 
-    @ViewChild('tabRef') tabRef:any;
+    @ViewChild('tabRef') tabRef: any;
 
-    constructor() {
+    documentClickListener: any;
+
+    items: any = [{
+        label: '关闭',
+        icon: 'fa-close',
+        command: (event) => {
+            this.closeCommand(event, 'current')
+        }
+    }, {
+        label: '关闭左侧标签',
+        icon: 'fa-close',
+        command: (event) => {
+            this.closeCommand(event, 'left')
+        }
+    }, {
+        label: '关闭右侧标签',
+        icon: 'fa-close',
+        command: (event) => {
+            this.closeCommand(event, 'right')
+        }
+    }, {
+        label: '关闭其他',
+        icon: 'fa-close',
+        command: (event) => {
+            this.closeCommand(event, 'other')
+        }
+    }, {
+        label: '关闭所有',
+        icon: 'fa-close',
+        command: (event) => {
+            this.closeCommand(event, 'all')
+        }
+    }];
+
+    // 右键菜单源
+    private cacheContextClickTarget: any;
+
+    constructor(public renderer: Renderer) {
 
     }
 
@@ -36,19 +77,38 @@ export class UITabView implements OnInit,AfterViewInit {
     }
 
     ngAfterViewInit(): void {
+        let contextMenu = $('#contextMenu');
+        this.documentClickListener = this.renderer.listenGlobal('body', 'click', () => {
+            contextMenu.hide();
+        });
+        $('ul[p-tabviewnav]').bind('contextmenu', (e) => {
+            let oe = e.originalEvent;
+            let target = oe.target;
+            if (target.nodeName === 'SPAN') {
+                this.cacheContextClickTarget = $(target);
+            } else {
+                this.cacheContextClickTarget = $(target).find('span');
+            }
+            contextMenu.css('left', oe.x);
+            contextMenu.css('top', oe.y);
+            contextMenu.show();
+            e.preventDefault();
+        });
+
     }
 
     handleChange(e) {
         this.tabChange.emit(e);
     }
-    handleClose(e){
+
+    handleClose(e) {
         this.tabClose.emit(e);
     }
 
     /**
      * 获取选中的tab
      */
-    findSelectedTab(){
+    findSelectedTab() {
         return this.tabRef.findSelectedTab();
     }
 
@@ -56,6 +116,66 @@ export class UITabView implements OnInit,AfterViewInit {
         this.activeIndex = index;
     }
 
+    /**
+     * 批量删除tab标签
+     * @param $event
+     * @param type
+     */
+    closeCommand($event, type) {
+        let text = this.cacheContextClickTarget.html();
+        let tabIndex = this.getTabIndexByName(text);
+        if (tabIndex === null) {
+            return;
+        }
+        switch (type) {
+            case 'current':
+                this.tabs.splice(tabIndex, 1);
+                break;
+            case 'left':
+                _.remove(this.tabs, function (n, index) {
+                    return index < tabIndex;
+                });
+                break;
+            case 'right':
+                _.remove(this.tabs, function (n, index) {
+                    return index > tabIndex;
+                });
+                break;
+            case 'other':
+                _.remove(this.tabs, function (n, index) {
+                    return index !== tabIndex;
+                });
+                break;
+            case 'all':
+                this.tabs.length = 0;
+                break;
+            default:
+                break;
+
+        }
+        $('#contextMenu').hide();
+    }
+
+    /**
+     * 标签页
+     * @param name
+     * @returns {any}
+     */
+    getTabIndexByName(name: string) {
+        let res = null;
+        this.tabs.forEach((tab, index) => {
+            if (tab.data && tab.data.name === name) {
+                res = index;
+            }
+        });
+        return res;
+    }
+
+    ngOnDestroy() {
+        if (this.documentClickListener) {
+            this.documentClickListener();
+        }
+    }
 }
 
 
@@ -64,7 +184,7 @@ export class UITabView implements OnInit,AfterViewInit {
         UITabView,
         UITabPanel,
     ],
-    imports: [TabViewModule, CommonModule],
+    imports: [TabViewModule, CommonModule, OverlayPanelModule, MenuModule],
     exports: [
         UITabView,
         UITabPanel,
